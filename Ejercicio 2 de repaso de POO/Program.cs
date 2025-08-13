72d80759c0eeab062a28b3e44ff1c709fd1e6de6
Carlos Gabriel Ortmann <o.carlosguiller@gmail.com>
2025-08-13 15:29:51 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio 2 de repaso de POO/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ejercicio_2_de_repaso_de_POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Ejercicio de repaso de POO/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ejercicio_1_de_repaso_de_POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
